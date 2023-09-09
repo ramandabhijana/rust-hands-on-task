1c7f90b56be8d3be7dff67353ea8f58ec58dac5b
ramandabhijana <78398153+ramandabhijana@users.noreply.github.com>
2023-09-09 11:51:03 +0800

    fix: replace str slice with owned string

--- week_1/src/main.rs
+++ week_1/src/main.rs
@@ -6,18 +6,18 @@ pub fn concatenate_strings(s1: &str, s2: &str) -> String {
 }
 
 fn main() {
-    let string1 = "Hello";
-    let string2 = "World";
+    let string1 = String::from("Hello");
+    let string2 = String::from("World");
 
-    let concatenated_strings = concatenate_strings(string1, string2);
+    let concatenated_strings = concatenate_strings(&string1, &string2);
 
     println!("concatenated_strings: {}", concatenated_strings);
 }
 
 #[test]
 fn should_concatenate_two_strings() {
-    let s1 = "Rise";
-    let s2 = "In";
-    let result = concatenate_strings(s1, s2);
+    let s1 = "Rise".to_owned();
+    let s2 = "In".to_owned();
+    let result = concatenate_strings(&s1, &s2);
     assert_eq!(result, String::from("RiseIn"));
 }
