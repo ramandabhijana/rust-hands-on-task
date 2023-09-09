8d45f2260d44962cd6d28c58f101946f2a4f57af
ramandabhijana <78398153+ramandabhijana@users.noreply.github.com>
2023-09-09 18:13:22 +0800

    feat: add calculate operation function

--- week_2/src/main.rs
+++ week_2/src/main.rs
@@ -5,6 +5,15 @@ enum Operation {
     Divide(f64, f64),
 }
 
+fn calculate(operation: &Operation) -> f64 {
+    match operation {
+        Operation::Add(a, b) => a + b,
+        Operation::Subtract(a, b) => a - b,
+        Operation::Multiply(a, b) => a * b,
+        Operation::Divide(a, b) => a / b,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
