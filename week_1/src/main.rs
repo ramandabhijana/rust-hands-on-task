64ac15bf4983e9eb4d1cd52c0641e27d478dadb7
ramandabhijana <78398153+ramandabhijana@users.noreply.github.com>
2023-09-09 11:43:07 +0800

    test: add unit test for concatenate_strings

--- week_1/src/main.rs
+++ week_1/src/main.rs
@@ -8,3 +8,11 @@ pub fn concatenate_strings(s1: &str, s2: &str) -> String {
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn should_concatenate_two_strings() {
+    let s1 = "Rise";
+    let s2 = "In";
+    let result = concatenate_strings(s1, s2);
+    assert_eq!(result, String::from("RiseIn"));
+}
